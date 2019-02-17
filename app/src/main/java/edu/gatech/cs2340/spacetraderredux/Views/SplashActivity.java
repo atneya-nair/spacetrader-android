be0cef80830ca0f3a3b69565e006c5d4bf7b7957
Seung Je Jung <xenoframium@gmail.com>
2019-02-17 17:14:20 -0500

    Fix splash activity crash.

--- app/src/main/java/edu/gatech/cs2340/spacetraderredux/Views/SplashActivity.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraderredux/Views/SplashActivity.java
@@ -1,4 +1,6 @@
 package edu.gatech.cs2340.spacetraderredux.Views;
 
-public class SplashActivity {
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
 }
