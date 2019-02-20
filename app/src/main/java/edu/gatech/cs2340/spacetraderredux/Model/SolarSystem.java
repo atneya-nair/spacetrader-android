6c630b30f5571175a1ed9a6f335dd9c5b00186d3
kangakum36 <kangakum@gmail.com>
2019-02-19 20:26:36 -0500

    M6 Solar Systems

--- app/src/main/java/edu/gatech/cs2340/spacetraderredux/Model/SolarSystem.java
+++ app/src/main/java/edu/gatech/cs2340/spacetraderredux/Model/SolarSystem.java
@@ -2,9 +2,38 @@ package edu.gatech.cs2340.spacetraderredux.Model;
 
 import android.graphics.Point;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class SolarSystem {
     private String name;
-    private Point location;
-    private TechLevel techLevel;
+    private static Point location;
     private Planet[] planets;
+
+    public SolarSystem(String name){
+        this.name = name;
+        this.planets = planets;
+        Point location = new Point(0,0);
+    }
+
+    public static void createSolarSystems(ArrayList<SolarSystem> solarSystems){
+        ArrayList<Integer> xcoordinatelist = new ArrayList<>(200);
+        ArrayList<Integer> ycoordinatelist = new ArrayList<>(200);
+        for(int i = 0; i < 200; i ++){
+            xcoordinatelist.add(i);
+            ycoordinatelist.add(i);
+        }
+        Collections.shuffle(xcoordinatelist);
+        Collections.shuffle(ycoordinatelist);
+        for(int i = 0; i < SolarSystemName.values().length; i++){
+             solarSystems.get(i).setLocation(xcoordinatelist.get(i), ycoordinatelist.get(i));
+
+        }
+
+
+    }
+
+    public void setLocation(int x, int y){
+        this.location = new Point(x, y);
+    }
 }
